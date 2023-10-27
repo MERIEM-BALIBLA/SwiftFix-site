71fb16089e905227aecb26227cf48b5c3dd1217e
BALIBLA MERIEM <balibla.meriem@gmail.com>
2023-10-27 10:25:43 +0200

    modification sur CSS

--- contact.js
+++ contact.js
@@ -1,79 +1,97 @@
-// var error = document.querySelector("#error")
-
-//  function valide() {
-//     var name = document.querySelector("#name").value;
-//     if(name.length == 0){
-//         error.innerHTML = "name's requiered";
-//         return false;
-//     }
-//     if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
-//         error.innerHTML = "rewrite your name";
-//         return false;
-//     }
-//         error.innerHTML = "valid";
-//         return true;
-// }
-
-document.querySelector("#contact-form").addEventListener("submit", function(e){
-    var error;
-    var input = document.getElementsByTagName('input');
-    var select = document.getElementsByTagName('select');
-    var textarea = document.getElementsByTagName('textarea');
-    for(var i=0;i< input.length;i++){
-        if(!input[i].velue)
-        error = "veuillez saisir ce champ";
-    }
-    if(erreur){
-        e.preventDefault();
-        erreur = document.getElementById("erreur");
-    }else{
-    alert('Formulaire a été bien enregistrer')
+var error = document.querySelector("#error")
+console.log(valide);
+function valide() {
+var name = document.querySelector("#name").value;
+if(name.length == 0){
+   error.innerHTML = "name is incorrect";
+   error.style.color = "red";
+   return false;
 }
-});
+if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+   error.innerHTML = "rewrite your name";
+   error.style.color = "red";
+   return false;
+}
+   error.innerHTML = "name's valid";
+   error.style.color = "green";
+   return true;
+}
+var errorE = document.querySelector("#error-email")
+function valideEmail(){
+var email = document.querySelector("#email").value;
+if(email.length == 0){
+   errorE.innerHTML = "email's incorrect";
+   errorE.style.color = "red";
+   return false;
+}
+if(!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)){
+    errorE.innerHTML = "email's incorrect";
+    errorE.style.color = "red";
+    return false;
+}
+errorE.innerHTML = "email's corecte";
+errorE.style.color = "green";
+return true;
+}
+var errorP = document.querySelector("#error-phone");
+function validateNumber() {
+var phone = document.querySelector("#phone").value;
+if (phone.length == 0) {
+    errorP.innerHTML = "Numéro's incorrect";
+    errorP.style.color = "red";
+    return false;
+}  
+if (!phone.match(/^\+212-[67]\d{8}$/)) {
+    errorP.innerHTML = "Numéro's incorrect";
+    errorP.style.color = "red";
+    return false;
+} 
+errorP.innerHTML = "Numéro's correct'";
+errorP.style.color = "green";
+return true;
+}
+var errorA = document.querySelector("#error-address");
+function validateAdress() {
+var address = document.querySelector("#address").value;
+if (address.length == 0) {
+    errorA.innerHTML = "Address's incorrect";
+    errorA.style.color = "red";
+    return false;
+}  
+if (!address.match(/^[\w\s]{10,40}$/)){
+    errorA.innerHTML = "Address's incorrect";
+    errorA.style.color = "red";
+    return false;
+} 
+errorA.innerHTML = "Address's correct";
+errorA.style.color = "green";
+return true;
+}
+var errorM = document.querySelector("#error-message");
+function validateMessage() {
+var message = document.querySelector("#message").value;
+if (message.length == 0) {
+    errorM.innerHTML = "add more information";
+    errorM.style.color = "red";
+    return false;
+}  
+if (!message.match(/^.{10,40}$/)) {
+    errorM.innerHTML = "add more information";
+    errorM.style.color = "red";
+    return false;
+} 
+errorM.innerHTML = "message valide";
+errorM.style.color = "green";
+return true;
+}
+var submiterror = document.getElementById('error-submit');
 
-    // for(var i=0;i< select.length;i++){
-    //     if(!select[i].velue)
-    //     error = "veuillez saisir ce champ";
-    // }
-
-    // for(var i=0;i< textarea.length;i++){
-    //     if(!textarea[i].velue)
-    //     error = "veuillez saisir ce champ";
-    // }
-    // // var name = document.querySelector("#name");
-    // var email = document.querySelector("#email");
-    // var PN = document.querySelector("#phone");
-    // var country = document.querySelector("#country");
-    // var address = document.querySelector("#address");
-    // var issues = document.querySelector("#issues");
-    // var message = document.querySelector("#message");
-
-    // if(!name.value){
-    //     erreur = "veuillez saisir votre nom";
-    // }
-    // if(!email.value){
-    //     erreur = "veuillez saisir votre email";
-    // }
-    // if(!PN.value){
-    //     erreur = "veuillez saisir votre numéro de téléphone";
-    // }
-    // if(!country.value){
-    //     erreur = "veuillez saisir country";
-    // }
-    // if(!address.value){
-    //     erreur = "veuillez saisir votre adresse";
-    // }
-    // if(!issues.value){
-    //     erreur = "veuillez saisir un probleme";
-    // }
-    // if(!message.value){
-    //     erreur = "veuillez écrire une petite description";
-    // }
-    // if(erreur){
-    //     e.preventDefault();
-    //     document.getElementById("erreur").innerHTML = erreur;
-    // }else{
-    // alert('Formulaire a été bien enregistrer')
-
-    // });
-    
+function formvalidet(){
+if(!valide() || !valideEmail() || !validateNumber() || !validateAdress() || !validateMessage()){
+    submiterror.innerHTML = 'Ensure that the information you provide is accurate and valid.';
+    submiterror.style.color = "red";
+    setTimeout(function(){submiterror.style.display = 'none';},3000);
+    return false;
+}
+alert("Le message a ete bien enregistre !");
+}
